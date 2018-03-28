e043e3ba1f14e8955616b526dd0c20f8af5c9150
drew-manuel <drew.manuel@jibo.com>
2018-03-28 10:04:12 -0400

    added create_default_config
    creates a default config file for the aws request if needed.

--- scripts/test_hub.py
+++ scripts/test_hub.py
@@ -1,6 +1,7 @@
 import paramiko
 import json
 import requests
+import boto3
 import os, sys
 import base64, datetime, hashlib, hmac
 
@@ -18,6 +19,23 @@ def copy_credentials_file(hostname, username, password, src_path, dst_path):
 	ftp_client = ssh_client.open_sftp()
 	ftp_client.get(src_path, dst_path)
 	ftp_client.close()
+
+
+"""
+	Creates a default config file for AWS
+	(aws_config.json)
+"""
+def create_default_config(path):
+	data = {}
+	data['method'] = 'GET'
+	data['service'] = 'ec2'
+	data['host'] = 'ec2.amazonaws.com'
+	data['region'] = 'us-east-1'
+	data['endpoint'] = 'https://ec2.amazonaws.com'
+
+	with open(path, 'w+') as file:
+		json.dump(data, file)
+
 
 
 """
@@ -46,9 +64,100 @@ def get_signature_key(key, date_stamp, region_name, service_name):
 
 
 """
-	Creates AWS session token
+	Manually signs the request
 """
-def create_token():
+def manual_signature(access_key, secret_key):
+
+	##########################
+	#    AWS REQUEST INFO    #
+	##########################
+
+	config_path = os.path.expanduser('~/jibo/Hub-Test/config/aws_config.json')
+
+	if not os.path.exists(config_path):
+		print("\nCreating default AWS config...")
+		create_default_config(config_path)
+		print("Done.\n")
+
+	aws_data = load_json(config_path)
+	method = aws_data['method']
+	service = aws_data['service']
+	host = aws_data['host']
+	aws_region = aws_data['region']
+	aws_endpoint = aws_data['endpoint']
+
+	request_parameters = ''
+
+	# Create a date for headers and the credential string
+	time = datetime.datetime.utcnow()
+	amz_date = time.strftime('%Y%m%d%dT%H%M%SZ')	# date with time
+	date_stamp = time.strftime('%Y%m%d')			# date w/o time, used in credential scope
+
+	##################################
+	#    CREATE CANONICAL REQUEST    #
+	##################################
+
+	uri = '/'																# create canonical URI (from domain to query), use '/' if no path
+	query = request_parameters												# must be sorted by name
+	canonical_headers = 'host:{}\nx-amz-date:{}\n'.format(host, amz_date)	# must be trimmed, lowercase, sorted in code point (low to high)
+	signed_headers = 'host;x-amz-date'										# lists headers in canonical_headers list, delimited with ; in alpha order
+	payload_hash = hashlib.sha256('').hexdigest()							# hash of the request body content, GET payload is empty string
+
+	# Combine elements to make request
+	canonical_request = '{}\n{}\n{}\n{}\n{}\n{}'.format(method, uri, query, canonical_headers, signed_headers, payload_hash)
+
+	###################################
+	#    CREATE THE STRING TO SIGN    #
+	###################################
+
+	algorithm = 'AWS4-HMAC-SHA256'
+	credential_scope = '{}/{}/{}/aws4_request'.format(date_stamp, aws_region, service)
+	hashed_req = hashlib.sha256(canonical_request).hexdigest()
+
+	# Combine elements to make string
+	string_to_sign = '{}\n{}\n{}\n{}'.format(algorithm, amz_date, credential_scope, hashed_req)
+
+	#############################
+	#    CALCULATE SIGNATURE    #
+	#############################
+
+	signing_key = get_signature_key(secret_key, date_stamp, aws_region, service)
+	encoded_string = string_to_sign.encode('utf-8')
+
+	# create the signature
+	signature = hmac.new(signing_key, encoded_string, hashlib.sha256).hexdigest()
+
+	##########################
+	#    ADD SIGNING INFO    #
+	##########################
+
+	authorization_header = '{} Credential={}/{}, SignedHeaders={}, Signature={}'.format(algorithm, access_key, credential_scope, signed_headers, signature)
+	headers = {'x-amz-date':amz_date, 'Authorization':authorization_header}
+
+	######################
+	#    SEND REQUEST    #
+	######################
+
+	request_url = '{}?{}'.format(aws_endpoint, query)
+
+	print('\n****** BEGIN REQUEST ******')
+	print('Request URL = ' + request_url)
+	response = requests.get(request_url, headers=headers)
+
+	print('\n****** RESPONSE ******')
+	print('Response code: %d\n' % response.status_code)
+	print(response.text)
+
+
+"""
+	Makes a request using the AWS SDK (boto3),
+	signature created automatically.
+"""
+def boto_request(access_key, secret_key):
+	return
+
+
+def main():
 
 	####################
 	#    LOGIN DATA    #
@@ -85,92 +194,8 @@ def create_token():
 		print("\nNo access key is available.\n")
 		sys.exit()
 
-
-	##########################
-	#    AWS REQUEST INFO    #
-	##########################
-
-	config_path = os.path.expanduser('~/jibo/Hub-Test/config/aws_config.json')
-
-	if not os.path.exists(config_path):
-		print("\nCreating default AWS config...")
-		create_default_config()
-		print("Done.\n")
-
-	aws_data = load_json(config_path)
-	method = aws_data['method']
-	service = aws_data['service']
-	host = aws_data['host']
-	aws_region = aws_data['region']
-	aws_endpoint = aws_data['endpoint']
-
-	request_parameters = ''
-
-	# Create a date for headers and the credential string
-	time = datetime.datetime.utcnow()
-	amz_date = time.strftime('%Y%m%d%dT%H%M%SZ')	# date with time
-	date_stamp = time.strftime('%Y%m%d')			# date w/o time, used in credential scope
-
-
-	##################################
-	#    CREATE CANONICAL REQUEST    #
-	##################################
-
-	uri = '/'																# create canonical URI (from domain to query), use '/' if no path
-	query = request_parameters												# must be sorted by name
-	canonical_headers = 'host:{}\nx-amz-date:{}\n'.format(host, amz_date)	# must be trimmed, lowercase, sorted in code point (low to high)
-	signed_headers = 'host;x-amz-date'										# lists headers in canonical_headers list, delimited with ; in alpha order
-	payload_hash = hashlib.sha256('').hexdigest()							# hash of the request body content, GET payload is empty string
-
-	# Combine elements to make request
-	canonical_request = '{}\n{}\n{}\n{}\n{}\n{}'.format(method, uri, query, canonical_headers, signed_headers, payload_hash)
-
-
-	###################################
-	#    CREATE THE STRING TO SIGN    #
-	###################################
-
-	algorithm = 'AWS4-HMAC-SHA256'
-	credential_scope = '{}/{}/{}/aws4_request'.format(date_stamp, aws_region, service)
-	hashed_req = hashlib.sha256(canonical_request).hexdigest()
-
-	# Combine elements to make string
-	string_to_sign = '{}\n{}\n{}\n{}'.format(algorithm, amz_date, credential_scope, hashed_req)
-
-
-	#############################
-	#    CALCULATE SIGNATURE    #
-	#############################
-
-	signing_key = get_signature_key(secret_key, date_stamp, aws_region, service)
-	encoded_string = string_to_sign.encode('utf-8')
-
-	# create the signature
-	signature = hmac.new(signing_key, encoded_string, hashlib.sha256).hexdigest()
-
-
-	##########################
-	#    ADD SIGNING INFO    #
-	##########################
-
-	authorization_header = '{} Credential={}/{}, SignedHeaders={}, Signature={}'.format(algorithm, access_key, credential_scope, signed_headers, signature)
-	headers = {'x-amz-date':amz_date, 'Authorization':authorization_header}
-
-
-	######################
-	#    SEND REQUEST    #
-	######################
-
-	request_url = '{}?{}'.format(aws_endpoint, query)
-
-	print('\n****** BEGIN REQUEST ******')
-	print('Request URL = ' + request_url)
-	response = requests.get(request_url, headers=headers)
-
-	print('\n****** RESPONSE ******')
-	print('Response code: %d\n' % response.status_code)
-	print(response.text)
+	create_default_config(os.path.expanduser('~/jibo/Hub-Test/config/aws_config.json'))
 
 
 if __name__ == "__main__":
-	create_token()
+	main()
