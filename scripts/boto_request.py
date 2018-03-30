f68df2f38505f750941e91ac347e4eb55a3855fe
drew-manuel <drew.manuel@jibo.com>
2018-03-30 11:21:47 -0400

    try to make an ec2 request

--- scripts/boto_request.py
+++ scripts/boto_request.py
@@ -10,12 +10,7 @@ from TestUtils.utils import *
 """
 	Tries to make a signed request to Elastic Search
 """
-def es_request():
-	cred_path = check_credentials()
-	cred_json = load_json(cred_path)
-	ACCESS_KEY = cred_json['accessKeyId']
-	SECRET_KEY = cred_json['secretAccessKey']
-	REGION = 'us-east-1'
+def es_request(ACCESS_KEY, SECRET_KEY, REGION):
 	SERVICE = 'es'
 	DOMAIN = 'stg-entrypoint'
 
@@ -23,7 +18,12 @@ def es_request():
 		print("\nCredentials are invalid.\n")
 		sys.exit()
 
-	session = boto3.Session(aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY, region_name=REGION)
+	session = boto3.Session(
+		aws_access_key_id=ACCESS_KEY, 
+		aws_secret_access_key=SECRET_KEY, 
+		region_name=REGION
+	)
+
 	credentials = session.get_credentials()
 
 	es_host = '{}.{}.{}.amazonaws.com'.format(DOMAIN, REGION, SERVICE)
@@ -48,8 +48,38 @@ def es_request():
 	print(es.info())
 
 
+"""
+"""
+def ec2_request(ACCESS_KEY, SECRET_KEY, REGION):
+	SERVICE = 'ec2'
+	
+	ec2 = boto3.client(
+		SERVICE, 
+		aws_access_key_id=ACCESS_KEY, 
+		aws_secret_access_key=SECRET_KEY, 
+		region_name=REGION
+	)
+
+	response = ec2.describe_instances()
+	print(response)
+
+
 def main():
-	es_request()
+	# get credentials
+	cred_path = check_credentials()
+	cred_json = load_json(cred_path)
+	ACCESS_KEY = cred_json['accessKeyId']
+	SECRET_KEY = cred_json['secretAccessKey']
+	REGION = 'us-east-1'
+
+	# check for empty creds
+	if ACCESS_KEY is None or SECRET_KEY is None:
+		print("\nCredentials are invalid.\n")
+		sys.exit()
+
+	# test requests
+	#es_request(ACCESS_KEY, SECRET_KEY, REGION)
+	ec2_request(ACCESS_KEY, SECRET_KEY, REGION)
 
 
 if __name__ == "__main__":
